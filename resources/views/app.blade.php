07110af1055d86c46005bd16181dd77c9971a4d8
Raphael Lu <leeraphael69@gmail.com>
2015-05-14 00:36:24 +0800

    Added task box function

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,9 @@
 	<title>ScrumAnyWhere</title>
 
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+	<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  	<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>  	
+	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 	@yield('head')
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
@@ -73,9 +76,5 @@
 		</div>
 	</div>
 </div>
-		
-	<!-- Scripts -->
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,5 +1,105 @@
 @extends('app')
 
+@section('head')
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+	<style>
+	  #taskBox { float: left; width: 40%; min-height: 12em; }
+	  .taskBox.custom-state-active { background: #eee; }
+	  .taskBox li { float: left; width: 120px; height: 120px; padding: 0.4em; margin: 0 0.4em 0.4em 0; text-align: center; }
+	  .taskBox li h5 { margin: 0 0 0.4em; cursor: move; }
+	  .taskBox li a { float: right; }
+	  .taskBox li img { width: 100%; cursor: move; }
+	 
+	  #doneArea { float: right; width: 60%; min-height: 18em; padding: 1%; }
+	  #doneArea h4 { line-height: 16px; margin: 0 0 0.4em; }
+	  #doneArea h4 .ui-icon { float: left; }
+	  #doneArea .taskBox h5 {  }
+  	</style>
+<script>
+  $(function() {
+    // there's the taskBox and the doneArea
+    var $taskBox = $( "#taskBox" ),
+      $doneArea = $( "#doneArea" );
+ 
+    // let the taskBox items be draggable
+    $( "li", $taskBox ).draggable({
+      cancel: "a.ui-icon", // clicking an icon won't initiate dragging
+      revert: "invalid", // when not dropped, the item will revert back to its initial position
+      containment: "document",
+      helper: "clone",
+      cursor: "move"
+    });
+ 
+    // let the doneArea be droppable, accepting the taskBox items
+    $doneArea.droppable({
+      accept: "#taskBox > li",
+      activeClass: "ui-state-highlight",
+      drop: function( event, ui ) {
+        taskIsDone( ui.draggable );
+      }
+    });
+ 
+    // let the taskBox be droppable as well, accepting items from the doneArea
+    $taskBox.droppable({
+      accept: "#doneArea li",
+      activeClass: "custom-state-active",
+      drop: function( event, ui ) {
+        taskIsToDo( ui.draggable );
+      }
+    });
+ 
+    // image deletion function
+    var undo_icon = "<a href='link/to/recycle/script/when/we/have/js/off' title='Recycle this image' class='ui-icon ui-icon-refresh'>Recycle image</a>";
+    function taskIsDone( $item ) {
+      $item.fadeOut(function() {
+        var $list = $( "ul", $doneArea ).length ?
+          $( "ul", $doneArea ) :
+          $( "<ul class='taskBox ui-helper-reset'/>" ).appendTo( $doneArea );
+ 
+        $item.find( "a.ui-icon-doneArea" ).remove();
+        $item.append( undo_icon ).appendTo( $list ).fadeIn(function() {
+          $item
+            .animate({ width: "120px", height: "120px" })
+            .find( "img" )
+              .animate({ height: "36px" });
+        });
+      });
+    }
+ 
+    // image recycle function
+    function taskIsToDo( $item ) {
+      $item.fadeOut(function() {
+        $item
+          .find( "a.ui-icon-refresh" )
+            .remove()
+          .end()
+          .css( "width", "120px")
+          .css( "height", "120px")
+          .find( "img" )
+          .end()
+          .appendTo( $taskBox )
+          .fadeIn();
+      });
+    }
+ 
+    // resolve the icons behavior with event delegation
+    $( "ul.taskBox > li" ).click(function( event ) {
+      var $item = $( this ),
+        $target = $( event.target );
+ 
+      if ( $target.is( "a.ui-icon-doneArea" ) ) {
+        taskIsDone( $item );
+      } else if ( $target.is( "a.ui-icon-refresh" ) ) {
+        taskIsToDo( $item );
+      }
+ 
+      return false;
+    });
+  });
+  </script>
+@endsection
+
+
 @section('main')
 	<div class="col-sm-12 column">		
 		{!! Form::open(['method'=>'DELETE', 'action' => ['projectController@destroy', $project->id], 'class'=> 'form-horizontal']) !!} 
@@ -68,6 +168,38 @@
 		</table>
 	</div>
 
-
+<div class="ui-widget ui-helper-clearfix">
+ 
+	<ul id="taskBox" class="taskBox ui-helper-reset ui-helper-clearfix">
+	  <li class="ui-widget-content ui-corner-tr">
+	    <h5 class="ui-widget-header">Task</h5>
+	    <div style="height=10px; width=10px;">content</div>   
+	    <a href="link/to/doneArea/script/when/we/have/js/off" title="Done" class="ui-icon ui-icon-doneArea">Delete image</a>
+	  </li>
+	  <li class="ui-widget-content ui-corner-tr">
+	    <h5 class="ui-widget-header">Task 2</h5>
+	    <div style="height=10px; width=10px;">content</div>
+	    <a href="link/to/doneArea/script/when/we/have/js/off" title="Done" class="ui-icon ui-icon-doneArea">Delete image</a>
+	  </li>
+	  <li class="ui-widget-content ui-corner-tr">
+	    <h5 class="ui-widget-header">Task 3</h5>
+	    <div style="height=10px; width=10px;">content</div>	    
+	    <a href="link/to/doneArea/script/when/we/have/js/off" title="Done" class="ui-icon ui-icon-doneArea">Delete image</a>
+	  </li>
+	  <li class="ui-widget-content ui-corner-tr">
+	    <h5 class="ui-widget-header">Task 4</h5>
+	    <div style="height=10px; width=10px;">content</div>
+	    <a href="link/to/doneArea/script/when/we/have/js/off" title="Done" class="ui-icon ui-icon-doneArea">Delete image</a>
+	  </li>
+	  <li class="ui-widget-content ui-corner-tr">
+	    <h5 class="ui-widget-header">Task 5</h5>
+	    <div style="height=10px; width=10px;">content</div>
+	    <a href="link/to/doneArea/script/when/we/have/js/off" title="Done" class="ui-icon ui-icon-doneArea">Delete image</a>
+	  </li>
+	</ul>
+	<div id="doneArea" class="ui-widget-content ui-state-default">
+	  <h4 class="ui-widget-header"><span class="ui-icon ui-icon-doneArea">Done</span> Done</h4>
+	</div>
+</div>
 @endsection
 
