de700b44144131ac8744d7df9f1171b4e54fb6dd
Raphael Lu <leeraphael69@gmail.com>
2015-05-25 12:30:22 +0800

    Added user profile link in app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -81,6 +81,8 @@
 						{{ Auth::user()->name }} <span class="caret"></span>
 					</a>
 					<ul class="dropdown-menu" role="menu">
+						<li><a href="{{ url('user/profile') }}">Profile</a></li>
+						<li class="divider"></li>
 						<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
 					</ul>
 				</li>
